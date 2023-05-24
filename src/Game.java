8ec3d01d7822f2d1d206f83c627a73429fc9cfdc
Jakub Matoga <kuba.29000@gmail.com>
2023-05-24 19:04:46 +0200

    poczatek game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,24 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tower Defense Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        // Tworzenie JLabel z tłem
+        JLabel backgroundLabel = new JLabel();
+        ImageIcon backgroundImage = new ImageIcon("path/to/background.jpg");
+        backgroundLabel.setIcon(backgroundImage);
+
+        // Ustalenie rozmiaru i położenia JLabel tak, aby wypełniał całe tło
+        backgroundLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+
+        // Dodanie JLabel do JFrame
+        frame.getContentPane().add(backgroundLabel);
+
+        frame.setVisible(true);
+    }
 }
