a3a01c4bfff2e9a397e1c7ab0c7ed0207b899306
Kraisu0 <krzysztofpopiela@poczta.fm>
2023-06-06 14:09:10 +0200

    aktualne

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,48 @@
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends JFrame
 {
+    private GameScreen gameScreen;
+    private BufferedImage img;
+
     public Game() {
 
-            setSize(800, 600);
+        importImgage();
+
+
+            setSize(1294, 998); //Robimy na takich wymairach
             setVisible(true);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLocationRelativeTo(null);
 
+            gameScreen = new GameScreen(img);
+            add(gameScreen);
+    }
+
+    private void importImgage() {
+        InputStream is = getClass().getResourceAsStream("/res/tlo.png");
+
+        try {
+            img = ImageIO.read(is);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("New game");
+
+        Game game = new Game();
 
     }
+
 }
 
 
---
+++ src/GameScreen.java
@@ -0,0 +1,42 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class GameScreen extends JPanel{
+
+    private Random random;
+    private BufferedImage img;
+
+    public GameScreen(BufferedImage img)
+    {
+        this.img = img;
+        random = new Random();
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        g.drawImage(img,0, 0,null);
+
+
+
+//        for(int x = 0; x < 40; x++) {
+//            for (int y = 0; y < 30; y++) {
+//                g.setColor(getRandomColor());
+//                g.fillRect(x * 32, y*32, 32, 32);
+//            }
+//        }
+    }
+
+    private Color getRandomColor()
+    {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
+
+    }
+}
