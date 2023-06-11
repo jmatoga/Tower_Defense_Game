9ff79743d64d6710c8758a42f55ddbd30e02baa4
Kraisu0 <krzysztofpopiela@poczta.fm>
2023-06-11 21:32:03 +0200

    11.06.2023 21:30 Do 22
    Signed-off-by: Kraisu0 <krzysztofpopiela@poczta.fm>

--- src/enemies/TurboHardUnit.java
+++ src/enemies/TurboHardUnit.java
@@ -5,6 +5,5 @@ import static help.Constants.Enemies.TURBO_HARD_UNIT;
 public class TurboHardUnit extends Enemy {
     public TurboHardUnit(float x, float y, int ID){
         super(x,y,ID,TURBO_HARD_UNIT);
-        setHealth(20);
     }
 }
